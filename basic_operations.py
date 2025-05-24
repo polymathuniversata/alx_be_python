20aa5d84f2ef9d1ceab24b835e92e2e358d737f5
Your Name <your-email@example.com>
2025-05-24 18:30:05 +0300

    Add basic_operations file

---
+++ basic_operations.py
@@ -0,0 +1,9 @@
+#A program to do basic operations +, -, *, / of two numbers; number1 and number 2
+number1 = 10 #Declare the first number
+number2 = 5 #Declare the second number 
+
+#Perform the operations addition, substraction and multiplication on the two numbers and print te results
+print ("Addition of", number1, "and", number2, "is", number1 + number2)
+print ("Substraction of", number1, "and", number2, "is", number1 - number2)
+print ("Multiplication of", number1, "and", number2, "is", number1 * number2)
+
